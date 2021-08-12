f3606788cd226dcadaeabd57fde5d867b123af10
Andrew Montalenti <andrew@parsely.com>
2021-08-12 12:01:46 -0400

    Test2 => Test3

--- webappfast.py
+++ webappfast.py
@@ -8,7 +8,7 @@ def hello_world():
 
 @app.route("/test")
 def hello_test():
-    return "<p>Hello, test!</p>"
+    return "<p>Hello, test2!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
