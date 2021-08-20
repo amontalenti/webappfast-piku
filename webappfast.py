76f2e4ef8cb8351bf4ac46437b150c632b31a7bd
Andrew Montalenti <andrew@parsely.com>
2021-08-20 12:29:46 -0400

    Add another endpoint

--- webappfast.py
+++ webappfast.py
@@ -7,8 +7,12 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/test")
-def hello_test():
-    return "<p>Hello, test2!</p>"
+def hello_test1():
+    return "<p>Hello, test1!</p>"
+
+@app.route("/test")
+def hello_test2():
+    return "<p>Hello, test4!</p>"
 
 # example docs
 if __name__ == "__main__":
